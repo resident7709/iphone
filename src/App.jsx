8bd6d0810adb6e05c5e5accd7eed4fa40936c79f
resident7709 <resident7709@gmail.com>
2024-03-16 21:11:09 +0300

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Highlights from './components/Highlights';
+
+const App = () => {
+  return (
+    <main className='bg-black'>
+      <Navbar /> <Hero /> <Highlights />
+    </main>
+  );
+};
+
+export default App;
