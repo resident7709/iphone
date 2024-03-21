597fccf7aaced84ff69de3229fa9b8926aad0a51
resident7709 <resident7709@gmail.com>
2024-03-21 21:36:08 +0300

    Add HowItWorks section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import * as Sentry from '@sentry/react';
 import Hero from './components/Hero';
 import Model from './components/Model';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Features from './components/Features';
 import Highlights from './components/Highlights';
 import HowItWorks from './components/HowItWorks';
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <main className='bg-black'>
       <Navbar /> <Hero /> <Highlights /> <Model /> <Features /> <HowItWorks />
+      <Footer />
     </main>
   );
 };
