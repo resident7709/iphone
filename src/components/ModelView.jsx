488bad2f7833b275e12f4eac23da69fa091e8528
resident7709 <resident7709@gmail.com>
2024-03-21 12:13:47 +0300

    Add IPhone.jsx

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -8,6 +8,7 @@ import {
 import { Suspense } from 'react';
 
 import Lights from './Lights';
+import IPhone from './IPhone';
 
 const ModelView = ({
   index,
@@ -28,7 +29,34 @@ const ModelView = ({
       <ambientLight intensity={0.3} />
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
       <Lights />
-      <Suspense fallback={<div>Loading</div>}></Suspense>
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large`}
+        position={[0, 0, 0]}>
+        <Suspense
+          fallback={
+            <Html>
+              <div>Loading..</div>
+            </Html>
+          }>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
     </View>
   );
 };
